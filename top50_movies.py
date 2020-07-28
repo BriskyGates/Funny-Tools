63832989d3fa80a5b395f6fbf5d04d07af16cc9f
BriskyGates <34669414+BriskyGates@users.noreply.github.com>
2020-07-28 21:10:34 +0800

    Update top50_movies.py
    correct some bugs about showing movies

--- top50_movies.py
+++ top50_movies.py
@@ -6,25 +6,30 @@ from lxml import etree
 from prettytable import PrettyTable
 
 latest_movies_url = "http://www.6vhao.tv/top.html"
-ROWS=10  # 总行数
+ROWS = 25  # 总行数
+COLUMNS = 3  # 总列数
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+
 
 def getTop50():
     """
-    在美观打印电影名时,做了取舍,直接将字段名变成每组第一个电影名
-    :return: 
+    打印电影名时,直接将第一行字段名变成每组第一个电影名
+    :return:
     """
-    response = requests.get(latest_movies_url)
+    response = requests.get(latest_movies_url, headers=headers)
     webContentEncode = response.apparent_encoding  # 获取网页内容的编码
     response.encoding = webContentEncode
     html = response.text
     selector = etree.HTML(html)
-    result = selector.xpath('//div[@class="listBox"]//tr//a/@title')  # 获取top50 的电影名字
+    result = selector.xpath('//div[@class="listBox"]'
+                            '//tr//a[@target="_blank"]'
+                            '/@title')  # 获取top50 的电影名字
     table = PrettyTable()
     index = 0
     for i in result[::ROWS]:
         rest_column = [j for j in result[index + 1:index + ROWS]]
         table.add_column(i, rest_column)
-        index += 5
+        index += ROWS
     print(table)
 
 
